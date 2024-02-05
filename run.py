7e64022b986aa7b42de23416179899d501acab0d
unknown <olivermandila@gmail.com>
2024-02-05 15:04:21 +0100

    feat: calculate new stock data and add it to stock worksheet

--- run.py
+++ run.py
@@ -86,13 +86,27 @@ def get_last_5_entries_sales():
     as a list of lists.
     """
     sales = SHEET.worksheet("sales")
-    # column = sales.col_values(3)
-    # print(column)
+    
     columns = []
     for ind in range(1,7):
         column = sales.col_values(ind)
-        columns.append(column)
-    print(columns)
+        columns.append(column[-5:])
+
+    return columns
+
+def calculate_stock_data(data):
+    """
+    Calculate the average stock for each item type, adding 10%
+    """
+    print("Calculating stock data...\n")
+    new_stock_data = []
+    for column in data:
+        int_column = [int(num) for num in column]
+        average = sum(int_column) / len(int_column)
+        stock_num = average * 1.1
+        new_stock_data.append(round(stock_num))
+    
+    return new_stock_data
 
 def main():
     """
@@ -104,8 +118,9 @@ def main():
     new_surplus_data = calculate_surplus_data(sales_data)
     print(f"Here is your new surplus data: {new_surplus_data}")
     update_worksheet(new_surplus_data, "surplus")
-
+    sales_columns = get_last_5_entries_sales()
+    stock_data = calculate_stock_data(sales_columns)
+    update_worksheet(stock_data, "stock")
 
 print("Welcome to Love Sandwiches Data Automation")
-# main()
-get_last_5_entries_sales()
+main()
